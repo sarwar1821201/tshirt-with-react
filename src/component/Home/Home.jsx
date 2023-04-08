113ffe5ebf73615375282af0eef10055b407772d
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-04-08 20:34:24 +0600

    initial data load

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const tshirts= useLoaderData();
+    //console.log(tshirts);
+
+    return (
+        <div>
+            <h2>This is home: {tshirts.length}</h2>
+        </div>
+    );
+};
+
+export default Home;
